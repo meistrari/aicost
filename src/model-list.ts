d1b17f5054614d80703bcd66aa0254c4a60606fa
henrycunh <henrycunh@users.noreply.github.com>
2024-07-06 06:04:09 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2024-07-06T00:12:22.293Z
-// Next update: 2024-07-06T06:12:22.293Z
+// Last updated: 2024-07-06T06:04:09.111Z
+// Next update: 2024-07-06T12:04:09.111Z
 
 export type AICostModelProvider = 'openai' | 'text-completion-openai' | 'azure' | 'anthropic' | 'mistral' | 'deepseek' | 'codestral' | 'text-completion-codestral' | 'groq' | 'friendliai' | 'vertex-ai' | 'vertex_ai-code-chat-models' | 'vertex_ai-anthropic_models' | 'cohere_chat' | 'cohere' | 'replicate' | 'openrouter' | 'ai21' | 'nlp_cloud' | 'aleph_alpha' | 'bedrock' | 'sagemaker' | 'ollama' | 'deepinfra' | 'perplexity' | 'anyscale' | 'cloudflare' | 'voyage' | 'databricks'
 
