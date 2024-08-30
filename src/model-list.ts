eb22a6e418397448a85befe3687b559118197104
henrycunh <henrycunh@users.noreply.github.com>
2024-08-30 18:04:22 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2024-08-30T12:05:32.997Z
-// Next update: 2024-08-30T18:05:32.997Z
+// Last updated: 2024-08-30T18:04:22.494Z
+// Next update: 2024-08-31T00:04:22.494Z
 
 export type AICostModelProvider = 'openai' | 'text-completion-openai' | 'azure' | 'azure_ai' | 'anthropic' | 'mistral' | 'deepseek' | 'codestral' | 'text-completion-codestral' | 'groq' | 'friendliai' | 'vertex-ai' | 'vertex_ai-code-chat-models' | 'vertex_ai-anthropic_models' | 'vertex_ai-llama_models' | 'vertex_ai-mistral_models' | 'vertex_ai-ai21_models' | 'cohere_chat' | 'cohere' | 'replicate' | 'openrouter' | 'ai21' | 'nlp_cloud' | 'aleph_alpha' | 'bedrock' | 'sagemaker' | 'ollama' | 'deepinfra' | 'perplexity' | 'fireworks_ai' | 'anyscale' | 'cloudflare' | 'voyage' | 'databricks'
 
@@ -256,6 +256,15 @@ export const AICostModelList = {
             "inputCost": 0.00003,
             "inputCostUnit": "token",
             "outputCost": 0.00006,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 16384,
+            "name": "ft:gpt-4o-2024-08-06",
+            "type": "chat",
+            "inputCost": 0.00000375,
+            "inputCostUnit": "token",
+            "outputCost": 0.000015,
             "outputCostUnit": "token"
         },
         {
