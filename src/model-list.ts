d8fe5af658681f214e5289f2e8c187ee508abcdc
henrycunh <henrycunh@users.noreply.github.com>
2024-10-04 00:14:54 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2024-10-03T18:04:38.960Z
-// Next update: 2024-10-04T00:04:38.960Z
+// Last updated: 2024-10-04T00:14:54.239Z
+// Next update: 2024-10-04T06:14:54.239Z
 
 export type AICostModelProvider = 'openai' | 'text-completion-openai' | 'azure' | 'azure_ai' | 'anthropic' | 'mistral' | 'deepseek' | 'codestral' | 'text-completion-codestral' | 'groq' | 'cerebras' | 'friendliai' | 'vertex-ai' | 'vertex_ai-code-chat-models' | 'vertex_ai-anthropic_models' | 'vertex_ai-llama_models' | 'vertex_ai-mistral_models' | 'vertex_ai-ai21_models' | 'cohere_chat' | 'cohere' | 'replicate' | 'openrouter' | 'ai21' | 'nlp_cloud' | 'aleph_alpha' | 'bedrock' | 'sagemaker' | 'together_ai' | 'ollama' | 'deepinfra' | 'perplexity' | 'fireworks_ai' | 'fireworks_ai-embedding-models' | 'anyscale' | 'cloudflare' | 'voyage' | 'databricks'
 
@@ -1652,7 +1652,7 @@ export const AICostModelList = {
         },
         {
             "maxTokens": 8192,
-            "name": "gemini-pro-flash",
+            "name": "gemini-flash-experimental",
             "type": "chat",
             "inputCost": 0,
             "inputCostUnit": null,
