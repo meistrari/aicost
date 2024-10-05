4c4494c3cfd0a7b0bae8d62e8ea9f31348ed611e
henrycunh <henrycunh@users.noreply.github.com>
2024-10-05 12:05:16 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2024-10-05T06:04:26.618Z
-// Next update: 2024-10-05T12:04:26.618Z
+// Last updated: 2024-10-05T12:05:15.593Z
+// Next update: 2024-10-05T18:05:15.593Z
 
 export type AICostModelProvider = 'openai' | 'text-completion-openai' | 'azure' | 'azure_ai' | 'anthropic' | 'mistral' | 'deepseek' | 'codestral' | 'text-completion-codestral' | 'groq' | 'cerebras' | 'friendliai' | 'vertex-ai' | 'vertex_ai-code-chat-models' | 'vertex_ai-anthropic_models' | 'vertex_ai-llama_models' | 'vertex_ai-mistral_models' | 'vertex_ai-ai21_models' | 'cohere_chat' | 'cohere' | 'replicate' | 'openrouter' | 'ai21' | 'nlp_cloud' | 'aleph_alpha' | 'bedrock' | 'sagemaker' | 'together_ai' | 'ollama' | 'deepinfra' | 'perplexity' | 'fireworks_ai' | 'fireworks_ai-embedding-models' | 'anyscale' | 'cloudflare' | 'voyage' | 'databricks'
 
@@ -374,42 +374,6 @@ export const AICostModelList = {
             "inputCostUnit": "token",
             "outputCost": 0,
             "outputCostUnit": null
-        },
-        {
-            "maxTokens": 65536,
-            "name": "openrouter/openai/o1-mini",
-            "type": "chat",
-            "inputCost": 0.000003,
-            "inputCostUnit": "token",
-            "outputCost": 0.000012,
-            "outputCostUnit": "token"
-        },
-        {
-            "maxTokens": 65536,
-            "name": "openrouter/openai/o1-mini-2024-09-12",
-            "type": "chat",
-            "inputCost": 0.000003,
-            "inputCostUnit": "token",
-            "outputCost": 0.000012,
-            "outputCostUnit": "token"
-        },
-        {
-            "maxTokens": 32768,
-            "name": "openrouter/openai/o1-preview",
-            "type": "chat",
-            "inputCost": 0.000015,
-            "inputCostUnit": "token",
-            "outputCost": 0.00006,
-            "outputCostUnit": "token"
-        },
-        {
-            "maxTokens": 32768,
-            "name": "openrouter/openai/o1-preview-2024-09-12",
-            "type": "chat",
-            "inputCost": 0.000015,
-            "inputCostUnit": "token",
-            "outputCost": 0.00006,
-            "outputCostUnit": "token"
         }
     ],
     "text-completion-openai": [
@@ -496,6 +460,42 @@ export const AICostModelList = {
         }
     ],
     "azure": [
+        {
+            "maxTokens": 65536,
+            "name": "azure/o1-mini",
+            "type": "chat",
+            "inputCost": 0.000003,
+            "inputCostUnit": "token",
+            "outputCost": 0.000012,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 65536,
+            "name": "azure/o1-mini-2024-09-12",
+            "type": "chat",
+            "inputCost": 0.000003,
+            "inputCostUnit": "token",
+            "outputCost": 0.000012,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 32768,
+            "name": "azure/o1-preview",
+            "type": "chat",
+            "inputCost": 0.000015,
+            "inputCostUnit": "token",
+            "outputCost": 0.00006,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 32768,
+            "name": "azure/o1-preview-2024-09-12",
+            "type": "chat",
+            "inputCost": 0.000015,
+            "inputCostUnit": "token",
+            "outputCost": 0.00006,
+            "outputCostUnit": "token"
+        },
         {
             "maxTokens": 4096,
             "name": "azure/gpt-4o",
@@ -2431,6 +2431,42 @@ export const AICostModelList = {
             "inputCost": 5.9e-7,
             "inputCostUnit": "token",
             "outputCost": 7.9e-7,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 65536,
+            "name": "openrouter/openai/o1-mini",
+            "type": "chat",
+            "inputCost": 0.000003,
+            "inputCostUnit": "token",
+            "outputCost": 0.000012,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 65536,
+            "name": "openrouter/openai/o1-mini-2024-09-12",
+            "type": "chat",
+            "inputCost": 0.000003,
+            "inputCostUnit": "token",
+            "outputCost": 0.000012,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 32768,
+            "name": "openrouter/openai/o1-preview",
+            "type": "chat",
+            "inputCost": 0.000015,
+            "inputCostUnit": "token",
+            "outputCost": 0.00006,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 32768,
+            "name": "openrouter/openai/o1-preview-2024-09-12",
+            "type": "chat",
+            "inputCost": 0.000015,
+            "inputCostUnit": "token",
+            "outputCost": 0.00006,
             "outputCostUnit": "token"
         },
         {
