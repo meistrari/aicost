7c34d2355141cece276ee5b3b702ecc7e7dfe328
henrycunh <henrycunh@users.noreply.github.com>
2025-02-21 06:04:53 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2025-02-21T00:15:19.142Z
-// Next update: 2025-02-21T06:15:19.142Z
+// Last updated: 2025-02-21T06:04:53.564Z
+// Next update: 2025-02-21T12:04:53.564Z
 
 export type AICostModelProvider = 'openai' | 'text-completion-openai' | 'azure' | 'azure_text' | 'azure_ai' | 'anthropic' | 'mistral' | 'deepseek' | 'codestral' | 'text-completion-codestral' | 'xai' | 'groq' | 'cerebras' | 'friendliai' | 'vertex-ai' | 'vertex_ai-code-chat-models' | 'vertex_ai-anthropic_models' | 'vertex_ai-llama_models' | 'vertex_ai-mistral_models' | 'vertex_ai-ai21_models' | 'cohere_chat' | 'cohere' | 'replicate' | 'openrouter' | 'ai21' | 'nlp_cloud' | 'aleph_alpha' | 'bedrock' | 'bedrock_converse' | 'sagemaker' | 'together_ai' | 'ollama' | 'deepinfra' | 'perplexity' | 'fireworks_ai' | 'fireworks_ai-embedding-models' | 'anyscale' | 'cloudflare' | 'voyage' | 'databricks' | 'sambanova'
 
@@ -1837,6 +1837,15 @@ export const AICostModelList = {
             "inputCost": 6e-7,
             "inputCostUnit": "token",
             "outputCost": 6e-7,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 128000,
+            "name": "cerebras/llama3.3-70b",
+            "type": "chat",
+            "inputCost": 8.5e-7,
+            "inputCostUnit": "token",
+            "outputCost": 0.0000012,
             "outputCostUnit": "token"
         }
     ],
