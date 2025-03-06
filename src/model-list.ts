19b2bbd54ae6fee5fbc0361d0c0239a113936210
henrycunh <henrycunh@users.noreply.github.com>
2025-03-06 12:06:01 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2025-03-06T06:05:12.304Z
-// Next update: 2025-03-06T12:05:12.304Z
+// Last updated: 2025-03-06T12:06:00.953Z
+// Next update: 2025-03-06T18:06:00.953Z
 
 export type AICostModelProvider = 'openai' | 'text-completion-openai' | 'azure' | 'azure_text' | 'azure_ai' | 'anthropic' | 'mistral' | 'deepseek' | 'codestral' | 'text-completion-codestral' | 'xai' | 'groq' | 'cerebras' | 'friendliai' | 'vertex-ai' | 'vertex_ai-code-chat-models' | 'vertex_ai-anthropic_models' | 'vertex_ai-llama_models' | 'vertex_ai-mistral_models' | 'vertex_ai-ai21_models' | 'cohere_chat' | 'cohere' | 'replicate' | 'openrouter' | 'ai21' | 'nlp_cloud' | 'aleph_alpha' | 'bedrock' | 'bedrock_converse' | 'sagemaker' | 'together_ai' | 'ollama' | 'deepinfra' | 'perplexity' | 'fireworks_ai' | 'fireworks_ai-embedding-models' | 'anyscale' | 'cloudflare' | 'voyage' | 'databricks' | 'sambanova'
 
@@ -1331,7 +1331,7 @@ export const AICostModelList = {
             "outputCostUnit": "token"
         },
         {
-            "maxTokens": 8192,
+            "maxTokens": 128000,
             "name": "claude-3-7-sonnet-latest",
             "type": "chat",
             "inputCost": 0.000003,
@@ -1340,7 +1340,7 @@ export const AICostModelList = {
             "outputCostUnit": "token"
         },
         {
-            "maxTokens": 8192,
+            "maxTokens": 128000,
             "name": "claude-3-7-sonnet-20250219",
             "type": "chat",
             "inputCost": 0.000003,
