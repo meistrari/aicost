9c17c06ebf20ad33bf14700723c26899c193fbc1
henrycunh <henrycunh@users.noreply.github.com>
2025-03-22 06:04:45 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2025-03-22T00:15:59.370Z
-// Next update: 2025-03-22T06:15:59.370Z
+// Last updated: 2025-03-22T06:04:44.692Z
+// Next update: 2025-03-22T12:04:44.693Z
 
 export type AICostModelProvider = 'openai' | 'text-completion-openai' | 'azure' | 'azure_text' | 'azure_ai' | 'anthropic' | 'mistral' | 'deepseek' | 'codestral' | 'text-completion-codestral' | 'xai' | 'groq' | 'cerebras' | 'friendliai' | 'vertex-ai' | 'vertex_ai-code-chat-models' | 'vertex_ai-anthropic_models' | 'vertex_ai-llama_models' | 'vertex_ai-mistral_models' | 'vertex_ai-ai21_models' | 'cohere_chat' | 'cohere' | 'replicate' | 'openrouter' | 'ai21' | 'nlp_cloud' | 'aleph_alpha' | 'bedrock' | 'bedrock_converse' | 'sagemaker' | 'together_ai' | 'ollama' | 'deepinfra' | 'perplexity' | 'fireworks_ai' | 'fireworks_ai-embedding-models' | 'anyscale' | 'cloudflare' | 'voyage' | 'databricks' | 'sambanova' | 'snowflake'
 
@@ -786,6 +786,15 @@ export const AICostModelList = {
         },
         {
             "maxTokens": 16384,
+            "name": "azure/gpt-4.5-preview",
+            "type": "chat",
+            "inputCost": 0.000075,
+            "inputCostUnit": "token",
+            "outputCost": 0.00015,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 16384,
             "name": "azure/gpt-4o",
             "type": "chat",
             "inputCost": 0.0000025,
@@ -1241,6 +1250,15 @@ export const AICostModelList = {
         {
             "maxTokens": 8191,
             "name": "azure_ai/mistral-small",
+            "type": "chat",
+            "inputCost": 0.000001,
+            "inputCostUnit": "token",
+            "outputCost": 0.000003,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 128000,
+            "name": "azure_ai/mistral-small-2503",
             "type": "chat",
             "inputCost": 0.000001,
             "inputCostUnit": "token",
