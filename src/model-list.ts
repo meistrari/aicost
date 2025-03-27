17ec68db245d131ee65ce384854b4845560b82fe
henrycunh <henrycunh@users.noreply.github.com>
2025-03-27 12:06:11 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2025-03-27T06:05:21.990Z
-// Next update: 2025-03-27T12:05:21.990Z
+// Last updated: 2025-03-27T12:06:11.128Z
+// Next update: 2025-03-27T18:06:11.128Z
 
 export type AICostModelProvider = 'openai' | 'text-completion-openai' | 'azure' | 'azure_text' | 'azure_ai' | 'anthropic' | 'mistral' | 'deepseek' | 'codestral' | 'text-completion-codestral' | 'xai' | 'groq' | 'cerebras' | 'friendliai' | 'vertex-ai' | 'vertex_ai-code-chat-models' | 'vertex_ai-anthropic_models' | 'vertex_ai-llama_models' | 'vertex_ai-mistral_models' | 'vertex_ai-ai21_models' | 'cohere_chat' | 'cohere' | 'replicate' | 'openrouter' | 'ai21' | 'nlp_cloud' | 'aleph_alpha' | 'bedrock' | 'bedrock_converse' | 'sagemaker' | 'together_ai' | 'ollama' | 'deepinfra' | 'perplexity' | 'fireworks_ai' | 'fireworks_ai-embedding-models' | 'anyscale' | 'cloudflare' | 'voyage' | 'databricks' | 'sambanova' | 'snowflake'
 
@@ -2712,6 +2712,15 @@ export const AICostModelList = {
             "name": "text-multilingual-embedding-002",
             "type": "embedding",
             "inputCost": 1e-7,
+            "inputCostUnit": "token",
+            "outputCost": 0,
+            "outputCostUnit": null
+        },
+        {
+            "maxTokens": 2048,
+            "name": "multimodalembedding",
+            "type": "embedding",
+            "inputCost": 8e-7,
             "inputCostUnit": "token",
             "outputCost": 0,
             "outputCostUnit": null
