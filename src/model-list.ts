a6a6cd4524e3856010d36dbb11183f664c83daf0
henrycunh <henrycunh@users.noreply.github.com>
2025-06-20 00:17:52 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2025-06-19T18:05:27.536Z
-// Next update: 2025-06-20T00:05:27.536Z
+// Last updated: 2025-06-20T00:17:52.572Z
+// Next update: 2025-06-20T06:17:52.572Z
 
 export type AICostModelProvider = 'openai' | 'watsonx' | 'azure' | 'text-completion-openai' | 'azure_text' | 'azure_ai' | 'anthropic' | 'mistral' | 'deepseek' | 'codestral' | 'text-completion-codestral' | 'xai' | 'groq' | 'cerebras' | 'friendliai' | 'vertex-ai' | 'vertex_ai-code-chat-models' | 'meta_llama' | 'vertex_ai-anthropic_models' | 'vertex_ai-llama_models' | 'vertex_ai-mistral_models' | 'vertex_ai-ai21_models' | 'cohere_chat' | 'cohere' | 'replicate' | 'openrouter' | 'ai21' | 'nlp_cloud' | 'aleph_alpha' | 'bedrock' | 'bedrock_converse' | 'sagemaker' | 'together_ai' | 'ollama' | 'deepinfra' | 'perplexity' | 'fireworks_ai' | 'fireworks_ai-embedding-models' | 'anyscale' | 'cloudflare' | 'voyage' | 'databricks' | 'sambanova' | 'snowflake' | 'nscale' | 'featherless_ai'
 
@@ -3271,9 +3271,9 @@ export const AICostModelList = {
             "maxTokens": 65535,
             "name": "gemini-2.5-flash-preview-05-20",
             "type": "chat",
-            "inputCost": 1.5e-7,
+            "inputCost": 3e-7,
             "inputCostUnit": "token",
-            "outputCost": 6e-7,
+            "outputCost": 0.0000025,
             "outputCostUnit": "token"
         },
         {
