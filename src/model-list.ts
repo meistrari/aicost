823576029f2cc4f98e520c976fa41c11fe3a034d
henrycunh <henrycunh@users.noreply.github.com>
2025-06-24 18:05:29 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2025-06-24T12:06:50.798Z
-// Next update: 2025-06-24T18:06:50.798Z
+// Last updated: 2025-06-24T18:05:28.563Z
+// Next update: 2025-06-25T00:05:28.563Z
 
 export type AICostModelProvider = 'openai' | 'watsonx' | 'azure' | 'text-completion-openai' | 'azure_text' | 'azure_ai' | 'anthropic' | 'mistral' | 'deepseek' | 'codestral' | 'text-completion-codestral' | 'xai' | 'groq' | 'cerebras' | 'friendliai' | 'vertex-ai' | 'vertex_ai-code-chat-models' | 'meta_llama' | 'vertex_ai-anthropic_models' | 'vertex_ai-llama_models' | 'vertex_ai-mistral_models' | 'vertex_ai-ai21_models' | 'cohere_chat' | 'cohere' | 'replicate' | 'openrouter' | 'ai21' | 'nlp_cloud' | 'aleph_alpha' | 'bedrock' | 'bedrock_converse' | 'sagemaker' | 'together_ai' | 'ollama' | 'deepinfra' | 'perplexity' | 'fireworks_ai' | 'fireworks_ai-embedding-models' | 'anyscale' | 'cloudflare' | 'voyage' | 'databricks' | 'sambanova' | 'snowflake' | 'nscale' | 'featherless_ai'
 
@@ -773,6 +773,24 @@ export const AICostModelList = {
             "inputCost": 1e-7,
             "inputCostUnit": "token",
             "outputCost": 4e-7,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 100000,
+            "name": "azure/o3-pro",
+            "type": "chat",
+            "inputCost": 0.00002,
+            "inputCostUnit": "token",
+            "outputCost": 0.00008,
+            "outputCostUnit": "token"
+        },
+        {
+            "maxTokens": 100000,
+            "name": "azure/o3-pro-2025-06-10",
+            "type": "chat",
+            "inputCost": 0.00002,
+            "inputCostUnit": "token",
+            "outputCost": 0.00008,
             "outputCostUnit": "token"
         },
         {
