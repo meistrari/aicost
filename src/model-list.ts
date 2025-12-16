6add1ffb68e1b66e52f221701b847cdd631d476a
henrycunh <henrycunh@users.noreply.github.com>
2025-12-16 06:06:42 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2025-12-16T00:19:45.359Z
-// Next update: 2025-12-16T06:19:45.359Z
+// Last updated: 2025-12-16T06:06:42.283Z
+// Next update: 2025-12-16T12:06:42.283Z
 
 export type AICostModelProvider = 'bedrock' | 'bedrock_converse' | 'anyscale' | 'azure' | 'azure_ai' | 'azure_text' | 'text-completion-openai' | 'cerebras' | 'vertex-ai' | 'nlp_cloud' | 'openai' | 'anthropic' | 'cloudflare' | 'vertex_ai-code-chat-models' | 'codestral' | 'cohere' | 'cohere_chat' | 'deepseek' | 'dashscope' | 'databricks' | 'deepinfra' | 'volcengine' | 'featherless_ai' | 'fireworks_ai-embedding-models' | 'fireworks_ai' | 'friendliai' | 'vertex_ai' | 'gradient_ai' | 'lemonade' | 'amazon_nova' | 'groq' | 'heroku' | 'hyperbolic' | 'ai21' | 'lambda_ai' | 'aleph_alpha' | 'meta_llama' | 'mistral' | 'moonshot' | 'morph' | 'nscale' | 'oci' | 'ollama' | 'openrouter' | 'ovhcloud' | 'perplexity' | 'publicai' | 'replicate' | 'sagemaker' | 'sambanova' | 'snowflake' | 'text-completion-codestral' | 'together_ai' | 'v0' | 'vercel_ai_gateway' | 'vertex_ai-anthropic_models' | 'vertex_ai-mistral_models' | 'vertex_ai-deepseek_models' | 'vertex_ai-ai21_models' | 'vertex_ai-llama_models' | 'vertex_ai-minimax_models' | 'vertex_ai-moonshot_models' | 'vertex_ai-openai_models' | 'vertex_ai-qwen_models' | 'voyage' | 'wandb' | 'watsonx' | 'xai' | 'zai'
 
@@ -6750,7 +6750,7 @@ export const AICostModelList = {
             "priceTier": 4
         },
         {
-            "maxTokens": 128000,
+            "maxTokens": 64000,
             "name": "claude-3-7-sonnet-20250219",
             "type": "chat",
             "inputCost": 0.000003,
@@ -6760,7 +6760,7 @@ export const AICostModelList = {
             "priceTier": 4
         },
         {
-            "maxTokens": 128000,
+            "maxTokens": 64000,
             "name": "claude-3-7-sonnet-latest",
             "type": "chat",
             "inputCost": 0.000003,
@@ -10735,12 +10735,13 @@ export const AICostModelList = {
         },
         {
             "maxTokens": 40960,
-            "name": "fireworks_ai/accounts/fireworks/models/qwen3-embedding-8b",
+            "name": "fireworks_ai/accounts/fireworks/models/",
             "type": "embedding",
-            "inputCost": 0,
-            "inputCostUnit": null,
+            "inputCost": 1e-7,
+            "inputCostUnit": "token",
             "outputCost": 0,
-            "outputCostUnit": null
+            "outputCostUnit": null,
+            "priceTier": 1
         },
         {
             "maxTokens": 4096,
