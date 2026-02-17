3576dc8cfe360d82d0985a5cd787d6ade192221c
henrycunh <henrycunh@users.noreply.github.com>
2026-02-17 00:25:23 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2026-02-16T18:12:51.899Z
-// Next update: 2026-02-17T00:12:51.899Z
+// Last updated: 2026-02-17T00:25:23.286Z
+// Next update: 2026-02-17T06:25:23.286Z
 
 export type AICostModelProvider = "bedrock" | "bedrock_converse" | "anyscale" | "azure" | "azure_ai" | "azure_text" | "text-completion-openai" | "cerebras" | "vertex-ai" | "nlp_cloud" | "openai" | "anthropic" | "cloudflare" | "vertex_ai-code-chat-models" | "codestral" | "cohere" | "cohere_chat" | "deepseek" | "dashscope" | "databricks" | "deepinfra" | "volcengine" | "featherless_ai" | "fireworks_ai-embedding-models" | "fireworks_ai" | "friendliai" | "vertex_ai" | "github_copilot" | "chatgpt" | "gigachat" | "gmi" | "gradient_ai" | "lemonade" | "amazon_nova" | "groq" | "heroku" | "hyperbolic" | "ai21" | "lambda_ai" | "aleph_alpha" | "meta_llama" | "minimax" | "mistral" | "moonshot" | "morph" | "nscale" | "oci" | "ollama" | "openrouter" | "ovhcloud" | "perplexity" | "publicai" | "replicate" | "sagemaker" | "sambanova" | "snowflake" | "text-completion-codestral" | "together_ai" | "v0" | "vercel_ai_gateway" | "vertex_ai-anthropic_models" | "vertex_ai-mistral_models" | "vertex_ai-deepseek_models" | "vertex_ai-ai21_models" | "vertex_ai-llama_models" | "vertex_ai-minimax_models" | "vertex_ai-moonshot_models" | "vertex_ai-zai_models" | "vertex_ai-openai_models" | "vertex_ai-qwen_models" | "voyage" | "wandb" | "watsonx" | "xai" | "zai" | "novita" | "llamagate" | "sarvam"
 
@@ -3314,7 +3314,7 @@ export const AICostModelList = {
         },
         {
             "maxTokens": 128000,
-            "name": "apac.anthropic.claude-opus-4-6-v1",
+            "name": "au.anthropic.claude-opus-4-6-v1",
             "type": "chat",
             "inputCost": 0.0000055,
             "inputCostUnit": "token",
@@ -20129,13 +20129,13 @@ export const AICostModelList = {
             "type": "chat",
             "inputCost": 0.000003,
             "inputCostUnit": "token",
-            "outputCost": 1.5e-7,
+            "outputCost": 0.000015,
             "outputCostUnit": "token",
             "cacheReadInputCost": null,
             "cacheReadInputCostUnit": null,
             "cacheCreationInputCost": null,
             "cacheCreationInputCostUnit": null,
-            "priceTier": 2
+            "priceTier": 4
         },
         {
             "maxTokens": 131072,
@@ -20185,13 +20185,13 @@ export const AICostModelList = {
             "type": "chat",
             "inputCost": 0.000003,
             "inputCostUnit": "token",
-            "outputCost": 1.5e-7,
+            "outputCost": 0.000015,
             "outputCostUnit": "token",
             "cacheReadInputCost": null,
             "cacheReadInputCostUnit": null,
             "cacheCreationInputCost": null,
             "cacheCreationInputCostUnit": null,
-            "priceTier": 2
+            "priceTier": 4
         },
         {
             "maxTokens": 4000,
