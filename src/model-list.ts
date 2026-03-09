12a9f567e863cdb1c54dec385fbcef9b7ba91201
henrycunh <henrycunh@users.noreply.github.com>
2026-03-09 12:12:27 +0000

    chore: update model list

--- src/model-list.ts
+++ src/model-list.ts
@@ -1,5 +1,5 @@
-// Last updated: 2026-03-09T06:23:13.317Z
-// Next update: 2026-03-09T12:23:13.317Z
+// Last updated: 2026-03-09T12:12:26.935Z
+// Next update: 2026-03-09T18:12:26.935Z
 
 export type AICostModelProvider = "bedrock" | "bedrock_converse" | "anyscale" | "azure" | "azure_ai" | "azure_text" | "text-completion-openai" | "cerebras" | "vertex-ai" | "nlp_cloud" | "openai" | "anthropic" | "cloudflare" | "vertex_ai-code-chat-models" | "codestral" | "cohere" | "cohere_chat" | "deepseek" | "dashscope" | "databricks" | "deepinfra" | "volcengine" | "featherless_ai" | "fireworks_ai-embedding-models" | "fireworks_ai" | "friendliai" | "vertex_ai" | "github_copilot" | "chatgpt" | "gigachat" | "gmi" | "gradient_ai" | "lemonade" | "amazon_nova" | "groq" | "heroku" | "hyperbolic" | "ai21" | "lambda_ai" | "aleph_alpha" | "meta_llama" | "minimax" | "mistral" | "moonshot" | "morph" | "nscale" | "nebius" | "oci" | "ollama" | "openrouter" | "ovhcloud" | "perplexity" | "publicai" | "replicate" | "sagemaker" | "sambanova" | "snowflake" | "text-completion-codestral" | "together_ai" | "v0" | "vercel_ai_gateway" | "vertex_ai-anthropic_models" | "vertex_ai-mistral_models" | "vertex_ai-deepseek_models" | "vertex_ai-ai21_models" | "vertex_ai-llama_models" | "vertex_ai-minimax_models" | "vertex_ai-moonshot_models" | "vertex_ai-zai_models" | "vertex_ai-openai_models" | "vertex_ai-qwen_models" | "voyage" | "wandb" | "watsonx" | "xai" | "zai" | "novita" | "llamagate" | "sarvam" | "bedrock_mantle"
 
@@ -10229,7 +10229,7 @@ export const AICostModelList = {
         {
             "maxTokens": 128000,
             "name": "gpt-5.4-pro",
-            "type": "chat",
+            "type": "responses",
             "inputCost": 0.00003,
             "inputCostUnit": "token",
             "outputCost": 0.00018,
@@ -10243,7 +10243,7 @@ export const AICostModelList = {
         {
             "maxTokens": 128000,
             "name": "gpt-5.4-pro-2026-03-05",
-            "type": "chat",
+            "type": "responses",
             "inputCost": 0.00003,
             "inputCostUnit": "token",
             "outputCost": 0.00018,
